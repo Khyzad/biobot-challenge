14efbf2aedb9db020e2f72ecfe6a1bec85583798
khyzad <lljruffin@gmail.com>
2023-02-03 03:28:46 -0800

    Add cors and update tsconfig

--- autocomplete/src/main.ts
+++ autocomplete/src/main.ts
@@ -1,20 +1,36 @@
 import express, { Express, Request, Response } from 'express';
 import { ShippingInfo } from './ShippingInfo';
 import { AutoCompleter } from './Trie';
+import cors from 'cors';
 
 const shipping_data: ShippingInfo[] = require('../KITS_SHIPPING_DATA.json');
 
 const app: Express = express();
-const port = 3000;
+const port = 3001;
 
 let cache: AutoCompleter = new AutoCompleter();
 cache.add(shipping_data);
+
+const corsOptions: cors.CorsOptions = {
+  allowedHeaders: [
+    'Origin',
+    'X-Requested-With',
+    'Content-Type',
+    'Accept',
+    'X-Access-Token',
+  ],
+  methods: 'GET,HEAD,OPTIONS,PUT,PATCH,POST,DELETE',
+  origin: '*',//'http://localhost:3000',
+  preflightContinue: false,
+};
+
+app.use(cors(corsOptions));
 
 app.get('/api/autocomplete/:query', (req: Request, res: Response) => {
   // limit the size of the result to 10
   let result = cache.find(req.params['query']);
   result.length = Math.min(result.length, 10);
-  
+
   res.send(result);
 });
 
